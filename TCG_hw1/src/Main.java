c803681275f68bcc7871686d2a11a082be9648af
Brian <b15132555@gmail.com>
2017-10-23 11:56:48 +0800

    not complete

--- TCG_hw1/src/Main.java
+++ TCG_hw1/src/Main.java
@@ -1,24 +1,65 @@
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String args[]){
-        parse("tcga2016-question.txt");
-
-
-    }
-
-    private int[][] parse(String inputName){
         String line;
+        
         try {
-            FileReader fileReader = new FileReader(inputName);
+        	boolean firstTime = true;
+        	int n = 0;
+            FileReader fileReader = new FileReader("tcga2016-question.txt");
             BufferedReader br = new BufferedReader(fileReader);
+            //find n
             while((line = br.readLine()) != null){
-
+            	if(line.charAt(0) == '$'){
+            		if(firstTime){
+            			firstTime = false;
+            		}else break;
+            	}else n++;
             }
+            
+            //pre-half = column, post-half = row
+            ArrayList<List> columnRow = new ArrayList<>();
+            
+            //parse 
+            fileReader = new FileReader("tcga2016-question.txt");
+            br = new BufferedReader(fileReader);
+            while((line = br.readLine()) != null){
+            	if(line.charAt(0) == '$'){
+            		for(int i=0; i<n; i++){
+            			line = br.readLine();
+//            			System.out.println(line);
+            			String[] ind = line.split("	");
+            			ArrayList<Integer> indexList = new ArrayList<>();
+            			for(String temp : ind){
+            				indexList.add(Integer.valueOf(temp));
+            			}
+            			columnRow.add(indexList);
+            		}
+            		break;
+            	}
+            }
+            
+//            //test parsing
+//            for(int i=0; i<n; i++){
+//            	for(int j=0; j<columnRow.get(i).size(); j++){
+//            		System.out.print(columnRow.get(i).get(j) + " ");
+//            	}
+//            	System.out.println();
+//            }
+            
+            br.close();
+            fileReader.close();
+            
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
+
 }
--- TCG_hw1/src/NaiveDFS.java
+++ TCG_hw1/src/NaiveDFS.java
@@ -1,3 +1,72 @@
-public class NaiveDFS {
+import java.util.ArrayList;
+import java.util.List;
 
+public class NaiveDFS {
+	
+	public ArrayList<List> inputs;
+	public int cNum;
+	public final int BLACK = 1;
+	public final int WHITE = 0;
+	
+	public NaiveDFS(ArrayList<List> inputs){
+		this.inputs = inputs;
+		cNum = inputs.size()/2;
+	}
+	
+	public void run(){
+		int nowInput = 0;
+		ArrayList<List> ans = new ArrayList<>();
+		//
+		for(int row=cNum; row<2*cNum; row++){
+			for(int i=0; i<inputs.get(row).size(); i++){
+				nowInput = (int) inputs.get(row).get(i);
+				ArrayList<Integer> ansRow = new ArrayList<>();
+				
+			}
+			
+		}
+	}
+	
+	public boolean check(ArrayList<List> ans, boolean partial){
+		int nowInput = 0;
+		int contiBlack = 0;
+		//check all col, i = col
+		for(int i=0; i<cNum; i++){
+			//k = row
+			int k=0;
+			//each col, check ans valid, j = input index
+			for(int j=0; j<cNum; j++){
+				nowInput = (int) inputs.get(i).get(j);
+				//iterate all elements in the col ,k = row
+				for(; k<cNum; k++){
+					int color = (int) ans.get(k).get(i);
+					if(color == BLACK){
+						contiBlack++;
+						if(contiBlack > nowInput) return false;
+					}else{  //when meet white, the previous contiBlack should equal 
+						if(contiBlack == nowInput){ //find next input
+							break;
+						}
+						//if not partial, contiBlack should not less than input.
+						if(!partial){
+							if(contiBlack != 0){
+								if(contiBlack < nowInput){
+									return false;
+								}
+							}
+						}
+						contiBlack = 0;
+					}
+				}
+			}
+		}
+		
+		//check last contiBlack if not partial check
+		if(!partial){
+			if(contiBlack < nowInput){ //find next input
+				return false;
+			}
+		}
+		return true;
+	}
 }
